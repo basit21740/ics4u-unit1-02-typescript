6a92747edc1f1f4ea482d35df53da2d62d55faf7
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-02-19 22:42:34 +0000

    added

--- src/Energy.ts
+++ src/Energy.ts
@@ -1,22 +1,27 @@
 /*
-* This program calculates how much energy will be generated
-*
-* @author  Abdul Basit Butt
-* @version 1.0
-* @since   2022-02-15
-*/
+ * This program calculates how much energy will be generated
+ *
+ * @author  Abdul Basit Butt
+ * @version 1.0
+ * @since   2022-02-15
+ */
 
-import promptSync from 'prompt-sync';
+import promptSync from "prompt-sync";
 const prompt = promptSync();
 
 const LIGHTSPEED = 2.998;
-const userInputString = prompt('Insert the mass of the object in Kg:');
-const userInput = parseFloat(userInputString)
-const energy = (userInput * LIGHTSPEED * LIGHTSPEED);
+const userInputString = prompt("Insert the mass of the object in Kg:");
+const userInput = parseFloat(userInputString);
+const energy = userInput * LIGHTSPEED * LIGHTSPEED;
 try {
-  if(isNaN(userInput)) throw "Invalid Input";
-    console.log("\nThe amount of energy the object would produce is:" + energy + " x10¹⁶ joules");
-    console.log("\nDone")
-  }finally {""}
+  if (isNaN(userInput)) throw "Invalid Input";
+  console.log(
+    "\nThe amount of energy the object would produce is:" +
+      energy +
+      " x10¹⁶ joules"
+  );
+  console.log("\nDone");
+} finally {
+  ("");
+}
 
-        
