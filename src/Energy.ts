f7879afa6512cbc96f59f0d4b9f3394ba3f73841
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-02-19 17:53:45 +0000

    added

---
+++ src/Energy.ts
@@ -0,0 +1,22 @@
+/*
+* This program calculates how much energy will be generated
+*
+* @author  Abdul Basit Butt
+* @version 1.0
+* @since   2022-02-15
+*/
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+const LIGHTSPEED = 2.998;
+
+var userInput = prompt('Insert the mass of the object in Kg:');
+let energy = (userInput * LIGHTSPEED * LIGHTSPEED);
+try {
+  if(isNaN(userInput)) throw "Invalid Input";
+    console.log("\nThe amount of energy the object would produce is:" + energy + " x10¹⁶ joules");
+   userInput = Number(userInput);
+  }finally {}
+console.log("\nDone")
+                      
